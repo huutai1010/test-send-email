0340ced2551286f29c65ef51969760ba4906aad6
huutai1010 <huutaia21010@gmail.com>
2022-07-18 16:17:01 +0700

    init project

---
+++ src/test/send/email/TestSendEmail.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test.send.email;
+
+/**
+ *
+ * @author maihuutai
+ */
+public class TestSendEmail {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        JavaMailUtil.sendMail("Nguyenhiep0456@gmail.com");
+    }
+    
+}
